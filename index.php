2a31084c0170c181ed649c70f2cb535f6268708f
Manish Sharma <65153077+codewins95@users.noreply.github.com>
2024-11-06 18:52:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
     <style>
-        #loader {
+       
+        /*Css For Form Loader Start*/
+        #loader2 {
             position: fixed;
             left: 0;
             top: 0;
@@ -22,213 +24,87 @@
             justify-content: center;
             align-items: center;
         }
-
-        .spinner {
+        
+        .spinner2 {
             border: 8px solid rgba(0, 0, 0, 0.1);
             border-top: 8px solid #000;
             border-radius: 50%;
             width: 50px;
             height: 50px;
-            animation: spin 1s linear infinite;
+            animation: spin2 1s linear infinite;
         }
-
-        @keyframes spin {
+        
+        @keyframes spin2 {
             0% {
                 transform: rotate(0deg);
             }
-
+        
             100% {
                 transform: rotate(360deg);
             }
         }
+        /*Css For Form Loader End*/
     </style>
 
 </head>
 
 <body>
 
-    <!--Header Section Start-->
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand navbar-logo border-0" href="index.php">
-                    <img src="./assets/img/logo.webp" width="300px" alt="logo">
-                </a>
-                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">Service</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact Us</a>
-                        </li>
-                    </ul>
-                    <a class="navbar-brand navbar-whatsapp border-0" target="_blank" href="https://api.whatsapp.com/send/?phone=%2B919821343163&text&type=phone_number&app_absent=0">
-                        <img src="./assets/img/whatsapp-chat-icon-1.webp" width="200px" alt="whatsappicon">
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <!--Header Section End-->
-
-    <!--Banner Section Start-->
-    <section class="container-fluid px-0">
-        <div class="row">
-            <img src="./assets/img/banner1.webp" class="img-fluid">
-        </div>
-        <div class="row marquee-bg mx-0">
-            <div>
-                <marquee><strong>GURUJI AYUSH TANTRIK BEST VASHIKARAN SERVICES IN DELHI</strong></marquee>
-            </div>
-        </div>
-    </section>
-    <!--Banner Section End-->
-
-    <!--Services Section Start-->
-    <div class="container py-4" id="services">
-        <h2 class="text-center">BEST ASTROLOGY SERVICE</h2>
-        <div class="row">
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service1.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Love Marriage Specialist</h5>
-                        <p class="card-text">In Love Marriage, families are not involved as it is usually the girl or boy to marry a person of his choice. Call Ayush guruji & get parents approval.</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service2.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Get Lost Love Back</h5>
-                        <p class="card-text">Getting lost love back become easy with help of astrology. Ayush guruji offering complete lost love problem solution with guranteed result.</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service3.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Love Problem Solution</h5>
-                        <p class="card-text">Don't let love problems affect your relationship. Keep it fresh, Act before its too late and get our love astrology services. Call Ayush guruji</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service4.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Resolving Family Disputes</h5>
-                        <p class="card-text">Insights from Ayush Guruji take time to reflect on the true nature of the conflict. Ask yourself, "What is really causing this tension?"</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service5.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Business Problem Solution</h5>
-                        <p class="card-text">Today the world has become a very competitive, which is at the level of business. All are running in the race to reach the number one spot.</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 content py-4">
-                <div class="content-box border border-dark rounded-3">
-                    <img src="./assets/img/service/service6.webp" class="card-img-top rounded-top" alt="img">
-                    <div class="card-body rounded-bottom">
-                        <h5 class="card-title">Health Problems Solutions</h5>
-                        <p class="card-text">The best and time honored method to do so is astrology as it can provide definite answers to any queries related to your business.</p>
-                        <a href="tel:+919821343163" class="btn btn-danger">Call Now</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--Services Section End-->
-
-    <!--Contact Form Section Start-->
-    <div class="contact_us" style="background:#f1f1f1">
-        <div class="container py-4">
-            <div class="row py-5">
-                <div class="col-lg-6 text-center my-5 contact_buttons" id="contact">
-                    <h3 class="fw-bold">Contact Us</h3>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-12 py-3">
-                                <a type="button" class="btn btn-danger" href="https://api.whatsapp.com/send/?phone=%2B919821343163&text&type=phone_number&app_absent=0" target="_blank">
-                                    <i class="fa-brands fa-whatsapp"></i>
-                                    Whatsapp Now
-                                </a>
-                            </div>
-                            <div class="col-12 py-3">
-                                <a type="button" class="btn btn-danger" href="tel:+919821343163" target="_blank">
-                                    <i class="fa-solid fa-phone"></i>
-                                    Request a Call
-                                </a>
-                            </div>
-                            <div class="col-12 py-3">
-                                <a type="button" class="btn btn-danger" href="mailto:blank" target="_blank">
-                                    <i class="fa-solid fa-envelope"></i>
-                                    Drop a Mail
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="contact-form">
-                        <form action="sendmail.php" method="POST" class="row g-3" onsubmit="formSubmit(event)">
-                            <div class="col-md-12">
-                                <label class="form-label">Full Name</label>
-                                <input type="text" name="name" class="form-control" aria-label="Full name" required="">
-                            </div>
-                            <div class="col-md-12">
-                                <label class="form-label">Email</label>
-                                <input type="email" name="email" class="form-control" required="">
-                            </div>
-                            <div class="col-12">
-                                <label class="form-label">Phone</label>
-                                <input type="tel" name="phone" class="form-control" required="">
-                            </div>
-                            <div class="col-12">
-                                <label class="form-label">Message</label>
-                                <textarea name="message" class="form-control" rows="3" placeholder="Message" required=""></textarea>
-                            </div>
-                            <div class="col-12">
-                                <button type="submit" class="btn bookon-call" id="submitBtn" style="cursor:pointer">
-                                    <svg class="svg-inline--fa fa-circle-arrow-right" aria-hidden="true" id="submitIcon" focusable="false" data-prefix="fas" data-icon="circle-arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                                        <path fill="currentColor" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM297 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L120 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l214.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L409 239c9.4 9.4 9.4 24.6 0 33.9L297 385z"></path>
-                                    </svg><!-- <i class="fa fa-arrow-circle-right" aria-hidden="true" id="submitIcon"></i> Font Awesome fontawesome.com -->
-                                    Submit Request
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--Contact Form Section End-->
-
     <!-- //loader section when form submit -->
-    <div id="loader" style="display: none;">
-        <div class="spinner"></div>
-        <span class="ms-3">Please Wait...</span>
-    </div>
-    <!-- //end loader section when form submit -->
+	<div id="loader2" style="display: none;">
+		<div class="spinner2"></div>
+		<span class="ms-3">Please Wait...</span>
+	</div>
+	<!-- //end loader section when form submit -->
+
+
+
+
+    
+    <form class="form-wrap" action="sendmail.php" method="POST" onsubmit="formSubmit(event)" id="contactForm">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="text" name="name" placeholder="Name*" id="name"
+                        required data-error="Please enter your name">
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="email" name="email" id="email" required
+                        placeholder="Email*" data-error="Please enter your email">
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="number" name="phone" id="phone" required
+                        placeholder="Phone Number*" data-error="Please enter your email">
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="text" name="subject" placeholder="Subject*" id="msg_subject" required data-error="Please enter your subject">
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group v1">
+                    <textarea name="message" id="message" placeholder="Your Messages.." cols="30" rows="10" required data-error="Please enter your message"></textarea>
+                    <div class="help-block with-errors"></div>
+                </div>
+            </div>                                            
+            <div class="col-md-12">
+                <button type="submit" id="submitBtn" class="btn style1">Send Message</button>
+                <div class="h3 text-center hidden"></div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </form>
+
+
 
 
     <a href="https://api.whatsapp.com/send/?phone=%2B919821343163&text&type=phone_number&app_absent=0" class="float" target="_blank">
@@ -237,9 +113,6 @@
     <a href="tel:+919821343163" class="call" target="_blank">
         <i class="fa-solid fa-phone my-call"></i>
     </a>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="./assets/js/script.js"></script>
