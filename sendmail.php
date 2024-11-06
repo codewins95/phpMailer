d59e28c3168693766865d75fc48f2948455a0797
Manish Sharma <65153077+codewins95@users.noreply.github.com>
2024-11-06 18:53:28 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -5,40 +5,53 @@ ini_set('display_errors', 1);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-
-    $fullName = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+    $fullName = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+    $subject = $_POST['subject'] ?? '';
+    $message = $_POST['message'] ?? '';
+    $inquiry_type = $_POST['inquiry_type'] ?? '';
+    $gender = $_POST['gender'] ?? '';
 
     try {
         $mail = new PHPMailer(true);
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'vinaypandey.hbs@gmail.com';
-        $mail->Password = 'lpmixbmnthiyetqq';
+        $mail->Username = ''; // your email
+        $mail->Password = ''; // your email password
         $mail->SMTPSecure = 'ssl';
         $mail->Port = 465;
 
-        $mail->setFrom('mks957678@gmail.com'); //your gmail
-        $mail->addAddress('mks957678@gmail.com');
+        $mail->setFrom('', ''); // your email
+        $mail->addAddress(''); // recipient email
         $mail->isHTML(true);
         $mail->Subject = 'New Enquiry Message';
-        $mail->Body    = '<h3>Hello, you got a new inquiry</h3>
-                          <h4>Full Name: ' . @$fullName . '</h4>
-                          <h4>Email : ' . @$email . '</h4>
-                          <h4>Phone Number: ' . @$phone . '</h4>                          
-                          <h4>Message: ' . @$message . '</h4>';
-        //$mail->SMTPDebug = 2;
 
+        // Build the email body
+        $mail->Body = '<h3>Hello, you got a new inquiry</h3>
+                       <h4>Full Name: ' . htmlspecialchars($fullName) . '</h4>
+                       <h4>Email: ' . htmlspecialchars($email) . '</h4>
+                       <h4>Phone Number: ' . htmlspecialchars($phone) . '</h4>';
 
+        if (!empty($subject)) {
+            $mail->Body .= '<h4>Subject: ' . htmlspecialchars($subject) . '</h4>';
+        }
+        if (!empty($message)) {
+            $mail->Body .= '<h4>Message: ' . htmlspecialchars($message) . '</h4>';
+        }
+        if (!empty($inquiry_type)) {
+            $mail->Body .= '<h4>inquiry_type: ' . htmlspecialchars($inquiry_type) . '</h4>';
+        }
+
+        if (!empty($gender)) {
+            $mail->Body .= '<h4>Gender: ' . htmlspecialchars($gender) . '</h4>';
+        }
 
         if ($mail->send()) {
             echo json_encode([
